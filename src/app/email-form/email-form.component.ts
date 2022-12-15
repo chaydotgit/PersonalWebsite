f988d290327c0f3d9f812892da480f7008d67a5d
Chayanne Rodriguez <chayanne624@gmail.com>
2022-12-14 17:03:55 -0800

    started working on contact form

--- src/app/email-form/email-form.component.ts
+++ src/app/email-form/email-form.component.ts
@@ -1,24 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-email-form',
   templateUrl: './email-form.component.html',
   styleUrls: ['./email-form.component.css']
 })
+
 export class EmailFormComponent implements OnInit {
-  FormData: FormGroup;
-  constructor( private builder : FormBuilder) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.FormData = this.builder.group({
-      FullName: new FormControl('', [Validators.required]),
-      Email: new FormControl('', [Validators.compose([Validators.required, Validators.email])]),
-      Comment: new FormControl('', [Validators.required])
-    });
   }
 }
 
-onsubmit(FormData) {
 
-}
