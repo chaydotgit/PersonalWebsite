2e16e10dce8a06822a1c9449164592a561d0639a
Chayanne Rodriguez <chayanne624@gmail.com>
2023-04-10 16:45:07 -0700

    added alt text to images and links for accessibility

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {ProjectsComponent} from "./projects/projects.component";
+import { HomeComponent } from "./home/home.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, title: "CR | Home" },
